216395fab8e40f25d1143ed042a39ff239e48127
lnc <lnc@din76000818.corp.capgemini.com>
2017-11-29 11:56:58 +0530

    sum value

--- day2-gitDemo/src/main/java/org/cap/demo/OrderClass.java
+++ day2-gitDemo/src/main/java/org/cap/demo/OrderClass.java
@@ -1,12 +1,15 @@
 package org.cap.demo;
 
 public class OrderClass {
-
+	int sum=0;
 	int arr[]={1,45,457};
 	public void print() {
-		for(int i=0;i<arr.length;i++)
+		for(int i=0;i<arr.length;i++){
+			sum+=arr[i];
 			System.out.println(arr[i]);
 	}
+		System.out.println(sum);
+		}
 	public void show() {
 		System.out.println("this is show");
 	}
